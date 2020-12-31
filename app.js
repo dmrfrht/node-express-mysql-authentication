e26c217ef29fa0497dc1fdc105ec4b47aee7bbaa
dmrfrht <dmrfrht1@gmail.com>
2020-12-31 17:21:51 +0300

    I made the database connection and created the users table

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express()
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'express_authentication'
+})
+
+db.connect((error) => {
+  if (error) console.error()
+  else console.log('mysql connected')
+})
 
 app.get('/', (req, res) => res.send('<h1>Home page</h1>'))
 
