96db5849b3c3a0fd515a6167f4afd4747465d380
dmrfrht <dmrfrht1@gmail.com>
2021-01-02 15:30:23 +0300

    create homepage and register page

--- app.js
+++ app.js
@@ -30,4 +30,12 @@ app.get('/', (req, res) => {
   res.render("index")
 })
 
+app.get('/register', (req, res) => {
+  res.render("register")
+})
+
+app.get('/login', (req, res) => {
+  res.render("login")
+})
+
 app.listen(3000, () => console.log('listening'))
